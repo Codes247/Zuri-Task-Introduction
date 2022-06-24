6f43813e61baeb6ac85ef9af305c389fc614831b
Codes247 <104468230+Codes247@users.noreply.github.com>
2022-06-24 13:05:26 +0100

    Add files via upload

---
+++ Zuri Js/intro.js
@@ -0,0 +1,4 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+console.log('Ikubaje Amos Ayokanmi(iZeus)')
+console.log('Height: 176cm');
+console.log('Country: Nigeria');
